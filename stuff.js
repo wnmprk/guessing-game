16a7c837bf55fd5077af7c45ec20c9fb4e1e84a0
wnmprk <wonmipark@gmail.com>
2015-10-14 21:08:33 -0400

    small edits in css

--- stuff.js
+++ stuff.js
@@ -1,12 +1,15 @@
 $(document).ready(function() {
 	var correct = Math.floor(Math.random()*100);
 	var totalGuesses = 5;
-	$('input').on('submit', function(inp)) { //separate function of both buttons
+	$(this).click(function()) { //separate function of both buttons
 		totalGuesses--;
-		while totalGuesses > 0 {
-			if (inp == correct) {
-				alert('Yay!');
-			}
-		}
+		//record guessed number
+		//check if guess if correct
+		//if correct, end game
+		//if wrong, allow user to keep guessing till they run of of guesses
+		//if close, alert "hot"
+		//if far, alert "cold"
+		//if last guess and incorrect ask user to "play/try again"
+		//if num was already guessed, alert user to guess new num and don't decrement totalGuesses
 	}
 })
