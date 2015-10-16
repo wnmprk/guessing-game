8b783bfb7b3bb97e8f6b177306018937116486e5
wnmprk <wonmipark@gmail.com>
2015-10-16 13:22:59 -0400

    js code added. edits on css.

--- README.rd
+++ README.rd
@@ -1,29 +1,29 @@
 Project Requirements:
 
-- When a game begins, there should be a random number generated between 1-100.
+0 Create a new game button that resets the game.
 
-- The user should have an input field where they can submit a guess.
+0 Change the background color, add an image, or do something creative when the user guesses the correct answer.
 
-- After the user submits a guess, indicate whether their guess is 'hot' or 'cold'. Let the user know if they need to guess higher or lower.
+0 When a game begins, there should be a random number generated between 1-100.
 
-- Allow the user to guess only a certain amount of times. When they run out of guesses let them know the game is over.
+0 The user should have an input field where they can submit a guess.
 
-- Feel free to use prompts to get user input on your first version.
+0 After the user submits a guess, indicate whether their guess is 'hot' or 'cold'. Let the user know if they need to guess higher or lower.
 
-- For the final version of your project, you'll need to create an HTML-based interface for getting user inputs and giving feedback on guesses.
+0 Allow the user to guess only a certain amount of times. When they run out of guesses let them know the game is over.
 
-- Validate inputs that they are real numbers between 1-100.
+0 Feel free to use prompts to get user input on your first version.
 
-- Create a new game button that resets the game.
+0 For the final version of your project, you'll need to create an HTML-based interface for getting user inputs and giving feedback on guesses.
 
-- Store all of the guesses and create a way to check if the guess is a repeat.
+0 Validate inputs that they are real numbers between 1-100.
 
-- Track the user's previous guess. Let them know if they are getting “hotter” or “colder” based on their previous guess.
+0 Store all of the guesses and create a way to check if the guess is a repeat.
 
-- Create a button that provides the answer (Give me a Hint).
+0 Track the user's previous guess. Let them know if they are getting “hotter” or “colder” based on their previous guess.
 
-- Submit the guess by pressing enter or clicking the submit button.
+0 Create a button that provides the answer (Give me a Hint).
 
-- After a user guesses a number keep a visual list of Hot and Cold answers that the user can see.
+0 Submit the guess by pressing enter or clicking the submit button.
 
-- Change the background color, add an image, or do something creative when the user guesses the correct answer.
+0 After a user guesses a number keep a visual list of Hot and Cold answers that the user can see.
--- stuff.js
+++ stuff.js
@@ -1,15 +1,113 @@
 $(document).ready(function() {
-	var correct = Math.floor(Math.random()*100);
-	var totalGuesses = 5;
-	$(this).click(function()) { //separate function of both buttons
-		totalGuesses--;
-		//record guessed number
-		//check if guess if correct
-		//if correct, end game
-		//if wrong, allow user to keep guessing till they run of of guesses
-		//if close, alert "hot"
-		//if far, alert "cold"
-		//if last guess and incorrect ask user to "play/try again"
-		//if num was already guessed, alert user to guess new num and don't decrement totalGuesses
-	}
-})
+	
+	//submits guess with enter/return key
+	$(document).bind('keypress', function(e) {
+        if(e.keyCode==13){
+             $('#submitGuess').trigger('click');
+        }
+    });
+
+	//initiate variables
+	var correct = Math.floor(Math.random()*100) + 1;
+	var attempts = 5;
+	var different = 0;
+	var guesses = [];
+
+	//function for submitting guesses
+	$('#submitGuess').on('click', function() {
+		//input value is current value
+		var current = Number($('input').val());
+
+		//when guess is correct. doesnt work?
+		if (current === correct && attempts > 0) {
+			$('body').css('background-image','url(img/winner.jpg)');
+			attempts = 0;
+			$('#hotOrCold').fadeIn().text("you guessed correctly!");
+			$('#remainder').fadeIn().text('yay!');
+		}
+		//checking to make sure input is valid
+		else if (!($.isNumeric(current)) || current > 100 || current < 1) {
+			$('#hotOrCold').fadeIn().text('guess an integer value between 1 and 100 please');
+		}
+		//when guess is incorrect
+		else if (current !== correct) {
+			//only push new guesses into array
+			if (guesses.indexOf(current) === -1) {
+				//push valid inputs into array and decrement attempts
+				guesses.push(current);
+				attempts--;
+				$('#remainder').text(attempts + " guesses left");
+				//when guess is too high
+				if (current > correct && attempts !== 0) {
+					var tooHigh = current - correct;
+					if (tooHigh > 50) {
+						$('#hotOrCold').fadeIn().text('colder than antarctica! guess lower...');
+						$('body').css("background-color","#87cefa");
+					}
+					else if (tooHigh >= 30) {
+						$('#hotOrCold').fadeIn().text('cold cold cold! guess lower...');
+						$('body').css("background-color","#9de24f");
+					}
+					else if (tooHigh >= 10) {
+						$('#hotOrCold').fadeIn().text('getting warmer! guess lower...');
+						$('body').css("background-color","#FFE303");						
+					}
+					else if (tooHigh >= 5) {
+						$('#hotOrCold').fadeIn().text('hot hot hot! guess lower...');	
+						$('body').css("background-color","#ffbd55");					
+					}
+					else {
+						$('#hotOrCold').fadeIn().text('hotter than death valley! guess lower...');
+						$('body').css("background-color","#ff6666");
+					}
+				}
+
+				//when guess is too low
+				else if (current < correct && attempts !== 0) {
+					var tooLow = correct - current;
+					if (tooLow >= 50) {
+						$('#hotOrCold').fadeIn().text('freezing cold! guess higher...');
+						$('body').css("background-color","#87cefa");
+					}
+					else if (tooLow >= 25) {
+						$('#hotOrCold').fadeIn().text('cold cold cold! guess higher...');
+						$('body').css("background-color","#9de24f");
+					}
+					else if (tooLow >= 10) {
+						$('#hotOrCold').fadeIn().text('getting warmer! guess higher...');
+						$('body').css("background-color","##FFE303");							
+					}
+					else if (tooLow >= 5) {
+						$('#hotOrCold').fadeIn().text('hot hot hot! guess higher...');
+						$('body').css("background-color","#ffbd55");						
+					}
+					else {
+						$('#hotOrCold').fadeIn().text('burning hot! guess higher...');
+						$('body').css("background-color","#ff6666");
+					}
+				}
+				else if (attempts < 1 && current !== correct) {
+					$('#hotOrCold').fadeIn().text('the answer is ' + correct + "!");
+					$('#remainder').fadeIn().text('game over');
+					$('body').css("background-color","black");
+				}
+			}
+			else {
+				$('#hotOrCold').fadeIn().text("you already guessed " + current);
+				$('#remainder').text(attempts + " guesses left");
+			}
+		}
+	});
+
+	//resets game
+	$('#resetGame').on('click', function() {
+		location.reload();
+	});
+
+	//gives hint/answer
+	$('#hint').on('click', function() {
+		var minus = Math.floor(Math.random()*100) + 1;
+		var hintNum = correct - minus;
+		$('#remainder').text(hintNum + " + " + minus);
+	});
+});
